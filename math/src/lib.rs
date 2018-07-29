365d0304092e982b300fba09025c79ff9a0ff838
Paul Schwarzwalder <pschwarz99@gmail.com>
2018-07-29 12:17:53 -0700

    Started Vector class

---
+++ math/src/lib.rs
@@ -0,0 +1,5 @@
+extern crate num;
+
+mod traits;
+
+pub mod vector;
---
+++ math/src/traits.rs
@@ -0,0 +1,79 @@
+use std::f32;
+
+pub trait SquareRoot {
+    type Output;
+
+    fn sqrt(self) -> Self::Output;
+}
+
+
+impl SquareRoot for f32 {
+    type Output = f32;
+
+    fn sqrt(self) -> f32 {
+        self.sqrt()
+    }
+}
+impl SquareRoot for f64 {
+    type Output = f64;
+
+    fn sqrt(self) -> f64 {
+        self.sqrt()
+    }
+}
+impl SquareRoot for i8 {
+    type Output = f32;
+
+    fn sqrt(self) -> f32 {
+        (self as f32).sqrt()
+    }
+}
+impl SquareRoot for i16 {
+    type Output = f32;
+
+    fn sqrt(self) -> f32 {
+        (self as f32).sqrt()
+    }
+}
+impl SquareRoot for i32 {
+    type Output = f32;
+
+    fn sqrt(self) -> f32 {
+        (self as f32).sqrt()
+    }
+}
+impl SquareRoot for i64 {
+    type Output = f64;
+
+    fn sqrt(self) -> f64 {
+        (self as f64).sqrt()
+    }
+}
+impl SquareRoot for u8 {
+    type Output = f32;
+
+    fn sqrt(self) -> f32 {
+        (self as f32).sqrt()
+    }
+}
+impl SquareRoot for u16 {
+    type Output = f32;
+
+    fn sqrt(self) -> f32 {
+        (self as f32).sqrt()
+    }
+}
+impl SquareRoot for u32 {
+    type Output = f32;
+
+    fn sqrt(self) -> f32 {
+        (self as f32).sqrt()
+    }
+}
+impl SquareRoot for u64 {
+    type Output = f64;
+
+    fn sqrt(self) -> f64 {
+        (self as f64).sqrt()
+    }
+}
