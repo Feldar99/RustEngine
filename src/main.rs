a0eadc3782aaa4f2ebbc705f4cf2a4f207da21b1
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2018-11-21 13:07:20 -0800

    Open empty black window

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,57 @@
-extern crate num;
-extern crate assert_approx_eq;
+#[macro_use] extern crate gfx;
+
+extern crate gfx_window_glutin;
+extern crate glutin;
+
+//use gfx::traits::FactoryExt;
+use gfx::Device;
+use gfx_window_glutin as gfx_glutin;
+
+pub type ColorFormat = gfx::format::Srgba8;
+pub type DepthFormat = gfx::format::DepthStencil;
+
+const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+
+gfx_defines! {
+    vertex Vertex {
+        pos: [f32; 2] = "a_Pos",
+        color: [f32; 3] = "a_Color",
+    }
+
+    pipeline pipe {
+        vbuf: gfx::VertexBuffer<Vertex> = (),
+        out: gfx::RenderTarget<ColorFormat> = "Target0",
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    let events_loop = glutin::EventsLoop::new();
+    let builder = glutin::WindowBuilder::new()
+        .with_title("Square Toy")
+        .with_dimensions(800, 800)
+        .with_vsync();
+    let (window, mut device, mut factory, mut main_color, mut main_depth) =
+        gfx_glutin::init::<ColorFormat, DepthFormat>(builder, &events_loop);
+
+    let mut encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
+
+    let mut running = true;
+    while running {
+        events_loop.poll_events(|glutin::Event::WindowEvent {window_id: _, event}| {
+            use glutin::WindowEvent::*;
+            match event {
+                KeyboardInput(_, _, Some(glutin::VirtualKeyCode::Escape), _)
+                | Closed => running = false,
+                Resized(_, _) => {
+                    gfx_glutin::update_views(&window, &mut main_color, &mut main_depth);
+                },
+                _ => (),
+            }
+        });
+
+        encoder.clear(&main_color, BLACK);
+        encoder.flush(&mut device);
+        window.swap_buffers().unwrap();
+        device.cleanup();
+    }
 }
